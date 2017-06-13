0687211c1a300f5d5be679a58c28595eff3426d3
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-14 03:30:54 +0900

    Goutte

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit411d069ad23b8d6f0198bd03a9487cb7
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit411d069ad23b8d6f0198bd03a9487cb7::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire411d069ad23b8d6f0198bd03a9487cb7($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire411d069ad23b8d6f0198bd03a9487cb7($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
