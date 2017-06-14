f926beccc96189d146065a081d384ab8edbc63e3
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-15 05:22:15 +0900

    PushAPI

--- pushMessage.php
+++ pushMessage.php
@@ -5,18 +5,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-// あなたのユーザーIDを入力して下さい
-// $userId = getenv('USER_ID');
+// あなたのユーザーIDを入力してください
+$userId = getenv('USER_ID');
 $message = 'Hello Push API';
 
-// メッセージをユーザーID宛てにPush
-$response = $bot->pushMessage(getenv('USER_ID'), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
+// メッセージをユーザーID宛にプッシュ
+$response = $bot->pushMessage($userId, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
 if (!$response->isSucceeded()) {
-  error_log('Failed!'. $responce->getHTTPStatus . ' ' . $responce->getRawBody());
+  error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
 }
 
 ?>
